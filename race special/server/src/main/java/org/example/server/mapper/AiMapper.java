d54711b20b321183401d226f866c35f335860ea8
zhh293 <928198963@qq.com>
2025-05-03 21:58:51 +0800

    Delete race special directory

--- race special/server/src/main/java/org/example/server/mapper/AiMapper.java
+++
@@ -1,7 +0,0 @@
-package org.example.server.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AiMapper {
-}
--- race special/server/src/main/java/org/example/server/service/AiService.java
+++
@@ -1,7 +0,0 @@
-package org.example.server.service;
-
-import javax.servlet.http.HttpSession;
-
-public interface AiService {
-    String question(String SessionId, String question) throws Exception;
-}
