9f802339978731b28b748c880cf070f4cffc6ca6
IHIRINA <2582067027@qq.com>
2025-05-06 11:07:22 +0800

    table

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/views/login/index.vue'
 import Register from '@/views/register/index.vue'
 import Home from '@/views/home.vue'
+import Table from '@/views/table/index.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,7 +22,11 @@ const router = createRouter({
       name: 'register',
       component: Register,
     },
-
+    {
+      path: '/table',
+      name: 'table',
+      component: Table,
+    }
   ]
 })
 
