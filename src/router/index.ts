f873f58ac2aa0e9dea016f5262fc9e616c4a7fab
IHIRINA <2582067027@qq.com>
2025-05-17 17:39:45 +0800

    home

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import Login from '@/views/login/index.vue'
 import Register from '@/views/register/index.vue'
 import Home from '@/views/home.vue'
 import Table from '@/views/table/index.vue'
+import TableAi from '@/views/ask/tableAi.vue'
+import ChatAi from '@/views/ask/chatAi.vue'
+import MusicAi from '@/views/ask/musicAi.vue'
+import My from '@/views/myself/my.vue'
+import Used from '@/views/myself/used.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +31,31 @@ const router = createRouter({
       path: '/table',
       name: 'table',
       component: Table,
+    },
+    {
+      path: '/tableAi',
+      name: 'tableAi',
+      component: TableAi,
+    },
+    {
+      path: '/chatAi',
+      name: 'chatAi',
+      component: ChatAi,
+    },
+    {
+      path: '/musicAi',
+      name: 'musicAi',
+      component: MusicAi,
+    },
+    {
+      path: '/my',
+      name: 'my',
+      component: My,
+    },
+    {
+      path: '/used',
+      name: 'used',
+      component: Used,
     }
   ]
 })
